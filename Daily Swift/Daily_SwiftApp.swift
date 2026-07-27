c81a8fdc9059d5943d26abe6ad12e9b7139c740a
Dragomir <d.mindrescu@moldcell.md>
2026-07-27 20:22:13 +0300

    Initial Commit

---
+++ Daily Swift/Daily_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_SwiftApp.swift
+//  Daily Swift
+//
+//  Created by Dragomir Mindrescu on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Daily_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
